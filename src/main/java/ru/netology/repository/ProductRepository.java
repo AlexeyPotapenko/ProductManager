a7a98b3985fe6314e127f18bd5695853c8e37ad5
AlexeyPotapenko <alesha_potapenko@mail.ru>
2021-11-29 16:59:44 +0300

    add tests

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,45 @@
+package ru.netology.repository;
+
+import ru.netology.domain.*;
+
+public class ProductRepository {
+    private Product[] products = new Product[0];
+
+    public void save(Product item) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = item;
+        products = tmp;
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public Product[] findById(int id) {
+        for (Product item : products) {
+            if (item.getId() == id) {
+                return new Product[]{item};
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : products) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+
+    public ProductRepository() {
+    }
+}
---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,72 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.*;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+    private ProductRepository repository = new ProductRepository();
+    private ProductManager manager = new ProductManager(repository);
+    private Book b1 = new Book(1, "Book1", 200, "Author1");
+    private Book b2 = new Book(2, "Book2", 300, "Author2");
+    private Book b3 = new Book(3, "Book3", 150, "Author3");
+    private Book b4 = new Book(4, "Book4", 450, "Author2");
+    private Book b5 = new Book(5, "Book5", 500, "Author1");
+    private Smartphone s1 = new Smartphone(1, "Smart1", 2000, "Samsung1");
+    private Smartphone s2 = new Smartphone(2, "Smart2", 5000, "Samsung2");
+    private Smartphone s3 = new Smartphone(3, "Smart3", 7500, "Samsung3");
+    private Smartphone s4 = new Smartphone(4, "Smart4", 9000, "Samsung4");
+    private Smartphone s5 = new Smartphone(5, "Smart5", 20000, "Samsung1");
+
+    @BeforeEach
+    void setUP() {
+        manager.add(b1);
+        manager.add(b2);
+        manager.add(b3);
+        manager.add(b4);
+        manager.add(b5);
+        manager.add(s1);
+        manager.add(s2);
+        manager.add(s3);
+        manager.add(s4);
+        manager.add(s5);
+    }
+
+    @Test
+    void searchByAutor() {
+        Book[] expected = new Book[]{b1, b5};
+        Product[] actual = manager.searchBy("Author1");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchByNameBook() {
+        Book[] expected = new Book[]{b3};
+        Product[] actual = manager.searchBy("Book3");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchByNameSmartphone() {
+        Smartphone[] expected = new Smartphone[]{s4};
+        Product[] actual = manager.searchBy("Samsung4");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchByProducer() {
+        Smartphone[] expected = new Smartphone[]{s1, s5};
+        Product[] actual = manager.searchBy("Samsung1");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchByNever() {
+        Smartphone[] expected = new Smartphone[]{};
+        Product[] actual = manager.searchBy(" ");
+        assertArrayEquals(expected, actual);
+    }
+}
